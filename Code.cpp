8e2ba5776b5f73a8e0d1895dc77748b06c0b57b8
DC-Code123 <Distinctcode123@gmail.com>
2025-04-04 16:50:23 +0300

    more updating

--- Code.cpp
+++ Code.cpp
@@ -8,59 +8,74 @@ using namespace std;
 int add(int x, int y) {
   return x + y;
 }
+
 int sub(int x, int y) {
   return x - y;
 }
+
 int multi(int x, int y) {
-   return x * y;
+  return x * y;
 }
+
 int div(int x, int y) {
-   return x / y;
+  if (y == 0) {
+    cout << "Error: Division by zero is not allowed." << endl;
+    return 0; // Return 0 to indicate an error
+  }
+  return x / y;
 }
 
+string div_print(int x, int y) {
+  if (y == 0) {
+    return "Error: Division by zero is not allowed.";
+  }
+  int quotient = x / y;
+  int remainder = x % y;
+  stringstream ss;
+  ss << "The quotient of " << x << " and " << y << " is: " << quotient
+     << ", and the remainder is: " << remainder;
+  return ss.str();
+}
 
-int main(){
+int main() {
   int x, y;
   char op;
   cout << "Enter first number: ";
   cin >> x;
   cout << "Enter second number: ";
   cin >> y;
-  cout << "which operation do you want to perform?: ";
-  cout << "Do you want to perform addition? (yes=y, no=n)";
+
+  cout << "Which operation do you want to perform?" << endl;
+  cout << "Do you want to perform addition? (yes=y, no=n): ";
   cin >> op;
+
   if (op == 'y' || op == 'Y') {
     cout << "The sum of " << x << " and " << y << " is: " << add(x, y) << endl;
-    int z = add(x, y);
-  }
-  else {
-    cout << "Do you want to perform subtraction? (yes=y, no=n)";
+  } else {
+    cout << "Do you want to perform subtraction? (yes=y, no=n): ";
     cin >> op;
     if (op == 'y' || op == 'Y') {
       cout << "The difference of " << x << " and " << y << " is: " << sub(x, y) << endl;
-      int z = sub(x, y);
-    }
-    else {
-      cout << "Do you want to perform multiplication? (yes=y, no=n)";
+    } else {
+      cout << "Do you want to perform multiplication? (yes=y, no=n): ";
       cin >> op;
       if (op == 'y' || op == 'Y') {
         cout << "The product of " << x << " and " << y << " is: " << multi(x, y) << endl;
-        int z = multi(x, y);
-      }
-      else {
-        cout << "Do you want to perform division? (yes=y, no=n)";
+      } else {
+        cout << "Do you want to perform division? (yes=y, no=n): ";
         cin >> op;
         if (op == 'y' || op == 'Y') {
-          cout << "The quotient of " << x << " and " << y << " is: " << div(x, y) << endl;
-          int z = div(x, y);
-          int remainder = x % y;
-          cout << "The remainder of " << x << " and " << y << " is: " << remainder << endl;
-        }
-        else {
+          if (y != 0) {
+            cout << div_print(x, y) << endl;
+          } else {
+            cout << "Error: Division by zero is not allowed." << endl;
+          }
+        } else {
           cout << "Invalid operation" << endl;
         }
       }
     }
   }
+
   return 0;
 }
