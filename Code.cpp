7c1be24e3325aa86829a0a6cafd032737465d365
DC-Code123 <Distinctcode123@gmail.com>
2025-04-04 23:35:50 +0300

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -19,17 +19,21 @@ int multi(int x, int y) {
 }
 
 int div(int x, int y) {
+
   if (y == 0) {
     cout << "Error: Division by zero is not allowed." << endl;
     return 0; // Return 0 to indicate an error
   }
+
   return x / y;
 }
 
 string div_print(int x, int y) {
+
   if (y == 0) {
     return "Error: Division by zero is not allowed.";
   }
+
   int quotient = x / y;
   int remainder = x % y;
   stringstream ss;
@@ -38,61 +42,70 @@ string div_print(int x, int y) {
   return ss.str();
 }
 
-int main() {
-  int x, y;
-  char op;
-  cout << "Enter first number: ";
-  cin >> x;
-  cout << "Enter second number: ";
-  cin >> y;
+int Basic_ops() {
+  cout<<"Welcome to The Specialized Calculator!"<<endl;
+  cout<<"This calculator can perform basic operations such as addition, subtraction, multiplication, and division."<<endl;
+  cout<<"It can also perform additional functions such as square root and power."<<endl;
+  cout<<"Do you want to perform basic operations? (yes=y, no=n): ";
+  char BasicFunction;
+  cin >> BasicFunction;
 
-  cout << "Which operation do you want to perform?" << endl;
-  cout << "Do you want to perform addition? (yes=y, no=n): ";
-  cin >> op;
-
-  if (op == 'y' || op == 'Y') {
-    cout << "The sum of " << x << " and " << y << " is: " << add(x, y) << endl;
-  } 
-    else {
-    cout << "Do you want to perform subtraction? (yes=y, no=n): ";
+  if(BasicFunction=='y'||BasicFunction=='Y'){
+    int x, y;
+    char op;
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+    cout << "Which operation do you want to perform?" << endl;
+    cout << "Do you want to perform addition? (yes=y, no=n): ";
     cin >> op;
+  
     if (op == 'y' || op == 'Y') {
-      cout << "The difference of " << x << " and " << y << " is: " << sub(x, y) << endl;
-    } 
-    else {
-      cout << "Do you want to perform multiplication? (yes=y, no=n): ";
-      cin >> op;
-      if (op == 'y' || op == 'Y') {
-        cout << "The product of " << x << " and " << y << " is: " << multi(x, y) << endl;
-      } 
+      cout << "The sum of " << x << " and " << y << " is: " << add(x, y) << endl;
+    }
+
       else {
-        cout << "Do you want to perform division? (yes=y, no=n): ";
+      cout << "Do you want to perform subtraction? (yes=y, no=n): ";
+      cin >> op;
+
+      if (op == 'y' || op == 'Y') {
+        cout << "The difference of " << x << " and " << y << " is: " << sub(x, y) << endl;
+      }
+
+      else {
+        cout << "Do you want to perform multiplication? (yes=y, no=n): ";
         cin >> op;
+
         if (op == 'y' || op == 'Y') {
-          if (y != 0) {
-            cout << div_print(x, y) << endl;
+          cout << "The product of " << x << " and " << y << " is: " << multi(x, y) << endl;
+        }
+
+        else {
+          cout << "Do you want to perform division? (yes=y, no=n): ";
+          cin >> op;
+
+          if (op == 'y' || op == 'Y') {
+
+            if (y != 0) {
+              cout << div_print(x, y) << endl;
+            } 
+
+            else {
+              cout << "Error: Division by zero is not allowed." << endl;
+            }
+
           } 
           else {
-            cout << "Error: Division by zero is not allowed." << endl;
+            cout << "Invalid operation" << endl;
           }
-        } 
-        else {
-          cout << "Invalid operation" << endl;
+
         }
+
       }
+
     }
-  }
-  char AdditionalFunction;
-  cout<<"Are you interested in performing additional functions? (yes=y, no=n): ";
-  cin>> AdditionalFunction;
-  if (AdditionalFunction == 'y'||AdditionalFunction == 'Y'){
-    void AdditionalFunction()
-  }
-  else{
-    cout<<"Thank you for using the calculator!"<<endl;
-    return 0;
+
   }
 
-  return 0;
 }
-void 
